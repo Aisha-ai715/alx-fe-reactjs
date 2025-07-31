fed68eed86d430324e031f75e67be260ca97b8c6
Aisha Elabass <aishaelabass@gmail.com>
2025-07-31 18:01:26 +0200

    Fix App.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
+import { useContext } from 'react';          
+import UserContext from './UserContext';     
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  const userData = useContext(UserContext);  
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>Welcome, {userData.name}</h2>
+      <UserInfo />
     </div>
   );
-};
+}
 
-export default UserProfile;
+export default ProfilePage;
