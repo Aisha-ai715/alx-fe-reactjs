b2f4041253d6fdd31be60df2e9733f2a22120064
Aisha Elabass <aishaelabass@gmail.com>
2025-07-31 13:42:36 +0200

    fix bugs

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ function MainContent() {
     <main style={{ backgroundColor: '#f9f9f9', padding: '20px', minHeight: '300px' }}>
       <h2 style={{ textAlign: 'center', color: '#333' }}>Welcome to My Page</h2>
       <p style={{ textAlign: 'center', color: '#666' }}>
-        I love to visit New York, South Africa, and Tokyo.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
