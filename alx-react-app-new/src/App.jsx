825ed34767a3f4adbedf863fcac8b1ea35d431b3
Aisha Elabass <aishaelabass@gmail.com>
2025-07-31 13:53:39 +0200

    Add file Counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,8 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-
-
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,12 +27,17 @@ function App() {
       <MainContent />
       <Footer />
 
-       <UserProfile 
-        name="Alice" 
-        age={25} 
-        bio="Loves hiking and photography" 
+      <UserProfile
+        name="Alice"
+        age={25}
+        bio="Loves hiking and photography"
       />
 
+      <div>
+        <h1 style={{ textAlign: 'center' }}>Counter App</h1>
+        <Counter />
+      </div>
+      
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
